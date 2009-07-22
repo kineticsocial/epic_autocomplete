6eaa993c0f6c8911b3ad74acc639f53f8a099d45
Kevin Pratt <kevin@ouelong.com>
2009-07-22 11:35:54 -0400

    Added some new install logic

--- install.rb
+++ install.rb
@@ -2,6 +2,6 @@
 require 'fileutils'
 
 puts "COPYING: public/javascripts/jquery.autocomplete.js to your public folder"
-cp "public/javascripts/jquery.autocomplete.js", "#{RAILS_ROOT}/public/javascripts/jquery.autocomplete.js" 
+cp "#{File.dirname(__FILE__)}/public/javascripts/jquery.autocomplete.js", "#{RAILS_ROOT}/public/javascripts/jquery.autocomplete.js" 
 puts "COPYING: public/stylesheets/jquery.autocomplete.css to your public folder"
-cp "public/stylesheets/jquery.autocomplete.css", "#{RAILS_ROOT}/public/stylesheets/jquery.autocomplete.css" 
+cp "#{File.dirname(__FILE__)}/public/stylesheets/jquery.autocomplete.css", "#{RAILS_ROOT}/public/stylesheets/jquery.autocomplete.css" 
